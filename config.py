632fde5eec9a88a19efb18815d8823f58ff7c017
vanguar <vanguar@ukr.net>
2024-11-24 20:40:50 +0100

    tocken

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # config.py
 
 from dotenv import load_dotenv
-load_dotenv()
 import os
 
 # Загрузка переменных окружения из .env файла
@@ -9,6 +8,7 @@ load_dotenv()
 
 # Получаем токен бота из переменных окружения
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+print(f"Загруженный TELEGRAM_TOKEN: {TELEGRAM_TOKEN}")
 
 # Настройки вебхука
 WEBHOOK_HOST = 'masteryodo.pythonanywhere.com'  # Замените на ваш действительный домен
@@ -19,3 +19,6 @@ WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_URL_PATH}"
 # Локальный адрес и порт для встроенного веб-сервера (не используются на PythonAnywhere)
 FLASK_HOST = '0.0.0.0'
 FLASK_PORT = 5000
+
+if not TELEGRAM_TOKEN:
+    raise ValueError("TELEGRAM_TOKEN не установлен! Проверьте файл .env и config.py.")
