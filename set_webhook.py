cdeeeed08a4a2272f67d5ca568cc7e15563dc0e3
vanguar <vanguar@ukr.net>
2024-11-25 21:58:04 +0100

    block try-exept

--- set_webhook.py
+++ set_webhook.py
@@ -11,13 +11,13 @@ dotenv_path = os.path.join(project_folder, '.env')
 # Загружаем переменные окружения из файла .env
 load_dotenv(dotenv_path)
 
-# Выводим переменные для проверки
-print(f"TELEGRAM_TOKEN: {os.getenv('TELEGRAM_TOKEN')}")
-print(f"WEBHOOK_URL: {os.getenv('WEBHOOK_URL')}")
-
 # Теперь импортируем bot и config
 from bot import application
 from config import WEBHOOK_URL
+
+# Выводим переменные для проверки
+print(f"TELEGRAM_TOKEN: {os.getenv('TELEGRAM_TOKEN')}")
+print(f"WEBHOOK_URL: {WEBHOOK_URL}")
 
 async def set_wb():
     await application.bot.set_webhook(WEBHOOK_URL)
