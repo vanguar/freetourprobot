c064973c64eb6f573b8a2d630fe9cc8708b68327
vanguar <vanguar@ukr.net>
2024-11-25 21:49:17 +0100

    block try-exept

--- set_webhook.py
+++ set_webhook.py
@@ -4,9 +4,20 @@ import os
 from dotenv import load_dotenv
 import asyncio
 
+# Определяем путь к директории проекта
 project_folder = os.path.expanduser('/home/MasterYodo/freetourprobot')
 dotenv_path = os.path.join(project_folder, '.env')
+
+# Загружаем переменные окружения из файла .env
 load_dotenv(dotenv_path)
 
-print(f"TELEGRAM_TOKEN: {os.getenv('TELEGRAM_TOKEN')}")
-print(f"WEBHOOK_URL: {os.getenv('WEBHOOK_URL')}")
+# Теперь импортируем bot и config
+from bot import application
+from config import WEBHOOK_URL
+
+async def set_wb():
+    await application.bot.set_webhook(WEBHOOK_URL)
+    print("Вебхук установлен успешно.")
+
+if __name__ == '__main__':
+    asyncio.run(set_wb())
