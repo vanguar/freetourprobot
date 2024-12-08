0fba114f142bf39011a2b7b12c986fddcf0a2e26
vanguar <vanguar@ukr.net>
2024-12-08 12:50:56 +0100

    Fix event loop issues and application initialization_08.12.2024

--- app.py
+++ app.py
@@ -11,23 +11,17 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-
-# Создаем и инициализируем приложение при старте
 telegram_app = create_application()
-telegram_app.initialize()  # Инициализируем приложение
-telegram_app.start()  # Запускаем приложение
 
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
-    """Обработчик вебхуков от Telegram."""
     if request.method == 'POST':
         try:
             update = Update.de_json(request.get_json(force=True), telegram_app.bot)
             logger.info("Получен webhook запрос")
             logger.info(f"Получен update: {update}")
             
-            # Запускаем обработку обновления через TaskQueue
-            telegram_app.update_queue.put(update)
+            telegram_app.process_update(update)
             return 'ok'
         except Exception as e:
             logger.error(f"Ошибка при обработке update: {e}")
@@ -37,6 +31,3 @@ def webhook():
 @app.route('/')
 def index():
     return 'Bot is running'
-
-if __name__ == '__main__':
-    app.run()
