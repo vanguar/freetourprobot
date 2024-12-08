2cff322ee088524f14e1695dcd24776ad026b353
vanguar <vanguar@ukr.net>
2024-12-08 12:33:32 +0100

    Fix event loop issues and application initialization_08.12.2024

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from telegram import Update
 from bot import create_application
 from config import WEBHOOK_URL, WEBHOOK_URL_PATH
 import logging
-import asyncio
-import nest_asyncio
-
-# Применяем nest_asyncio для вложенных event loops
-nest_asyncio.apply()
+from asgiref.sync import async_to_sync
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -19,17 +15,19 @@ app = Flask(__name__)
 telegram_app = create_application()
 
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
-async def webhook():
-    try:
-        update = Update.de_json(request.get_json(force=True), telegram_app.bot)
-        logger.info(f"Получен webhook запрос")
-        logger.info(f"Получен update: {update}")
-        
-        await telegram_app.process_update(update)
-        return 'ok'
-    except Exception as e:
-        logger.error(f"Ошибка при обработке update: {e}")
-        return 'Error processing update', 500
+def webhook():
+    if request.method == 'POST':
+        try:
+            update = Update.de_json(request.get_json(force=True), telegram_app.bot)
+            logger.info("Получен webhook запрос")
+            logger.info(f"Получен update: {update}")
+
+            async_to_sync(telegram_app.process_update)(update)
+            return 'ok'
+        except Exception as e:
+            logger.error(f"Ошибка при обработке update: {e}")
+            return 'Error processing update', 500
+    return 'ok'
 
 @app.route('/')
 def index():
